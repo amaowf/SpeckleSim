3883b0c4a3afca2bc555a7fd22c06a74dd7a69e1
ac103757 <liwei.fu@ito.uni-stuttgart.de>
2021-06-09 15:58:01 +0200

    Add files via upload

---
+++ lib_sie_tri_disc_mod.f90
@@ -0,0 +1,34 @@
+!  Rune_Code.f90 
+!
+!  FUNCTIONS:
+!  Rune_Code - Entry point of console application.
+
+!****************************************************************************
+!
+!  PROGRAM: Rune_Code
+!
+!  PURPOSE:  Entry point for the console application.
+!
+!****************************************************************************
+
+module lib_sie_tri_disc_mod
+!
+! Module defining the discretization data structure and
+! subroutines for filling the structure with data using
+! different discretization algorithms.
+
+   use input_parameters
+   use Gauss_Quadrature_Fast     
+   use lib_sie_type_function
+	use lib_sie_data_container
+	    
+   implicit none	 
+	contains
+	 
+
+	
+
+        
+
+	
+end module lib_sie_tri_disc_mod
