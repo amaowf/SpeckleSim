9bda4e6cd1a935b4eb1107838476dbe352416030
ac103757 <liwei.fu@ito.uni-stuttgart.de>
2021-12-03 15:23:45 +0100

    Delete build.sh

--- build.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-cpu_cores=$(nproc --all)
-FoBiS.py build -fc "gfortran-8 -cpp -fopenmp -ffree-line-length-none -frecursive" -j $cpu_cores -ed FoggySim src/lib/libmath_generated_functions src/lib/libmath/src/math/lib_math_wigner.f90 .ipynb_checkpoints
